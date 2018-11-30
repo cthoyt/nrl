7c888a403c4400325ca5740323047a11c4115c27
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-11-30 17:33:09 +0100

    Add karate club graph

--- tests/constants.py
+++ tests/constants.py
@@ -10,14 +10,18 @@ from nrl.io import read_ncol_graph
 
 __all__ = [
     'HERE',
-    'TEST_NETWORK_PATH',
+    'TEST_HIPPIE_NETWORK_PATH',
+    'KARATE_CLUB_PATH',
     'get_test_network',
 ]
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-TEST_NETWORK_PATH = os.path.join(HERE, 'hippie_current.edgelist')
+
+RESOURCES_DIRECTORY = os.path.join(HERE, 'resources')
+KARATE_CLUB_PATH = os.path.join(RESOURCES_DIRECTORY, 'karate.edgelist')
+TEST_HIPPIE_NETWORK_PATH = os.path.join(RESOURCES_DIRECTORY, 'hippie_current.edgelist')
 
 
 def get_test_network() -> igraph.Graph:
     """Get the test network."""
-    return read_ncol_graph(TEST_NETWORK_PATH)
+    return read_ncol_graph(KARATE_CLUB_PATH)
