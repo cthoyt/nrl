fd815641e62b882843b8995dabfd2f1a8d4ae2d6
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-12-04 22:18:17 +0100

    Fix spacing

--- src/nrl/__init__.py
+++ src/nrl/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
 """Network representation learning (NRL) in Python."""
-from nrl.algorithm import deepwalk, gat2vec, node2vec, random_walk, word2vec
 
-# __all__ = []
+from nrl.algorithm import deepwalk, gat2vec, node2vec, random_walk, word2vec
