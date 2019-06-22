cd4b13afba683ef03cad9786f0fa310eeb0c9484
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-06-22 21:24:28 +0200

    Update __init__.py

--- src/nrl/model/__init__.py
+++ src/nrl/model/__init__.py
@@ -5,11 +5,12 @@
 from .deepwalk import DeepWalkModel
 from .gat2vec import Gat2VecUnsupervisedModel
 from .node2vec import Node2VecModel
-from .util import WalkerModel
+from .util import WalkerModel, Word2VecParameters
 
 __all__ = [
+    'Word2VecParameters',
     'WalkerModel',
     'DeepWalkModel',
     'Gat2VecUnsupervisedModel',
-    'Node2VecModel'
+    'Node2VecModel',
 ]
